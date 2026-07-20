e6ca0306d4178ea6481fe082f95b63cd0fb5c32e
AadhityaKumar <123118401+AadhityaKumar@users.noreply.github.com>
2026-07-19 21:19:05 -0500

    Update library_client.py

--- python_grpc/library_client.py
+++ python_grpc/library_client.py
@@ -8,10 +8,6 @@ import librarySystem_pb2_grpc
 
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    #print("Will try to greet world ...")
     with grpc.insecure_channel("grpc-server:50051") as channel:
         stub = librarySystem_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(librarySystem_pb2.HelloRequest(arg1=5, arg2=4))
